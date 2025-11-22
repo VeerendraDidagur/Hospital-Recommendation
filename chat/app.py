9c8f85a1f80c4c33d7d367f6f939199455d57523
VeerendraDidagur <1ep23is181@eastpoint.ac.in>
2025-11-22 12:52:10 +0530

    Update app.py

--- chat/app.py
+++ chat/app.py
@@ -18,20 +18,7 @@ hospitals = [
     {"id": "h6", "name": "V Care Hospital", "location": "Bangalore", "rating": 4.4, "address":"K R Puram, Bangalore"},
     {"id": "h7", "name": "National Care Hospital", "location": "Hyderabad", "rating": 4.4, "address":"12 Banjara Hills, Hyderabad"},
 ]
-with app.app_context():
-    east_point = Hospital(
-        name="East Point Hospital",
-        address="3P39+M3 Bengaluru, Karnataka",
-        city="Bangalore",
-        rating=4.8,
-        specialization="Multi-specialty",
-        cost_category=2,
-        specialists=["Cardiology", "Neurology", "Orthopedics", "Dermatology"],
-        symptoms=["Chest Pain", "Headache", "Skin Allergy", "Joint Pain"]
-    )
-    db.session.add(east_point)
-    db.session.commit()
-    print("Hospital added successfully!")
+
 
 
 # Home/search page
@@ -98,6 +85,22 @@ class User(db.Model):
     password = db.Column(db.String(200), nullable=False)
 with app.app_context():
     db.create_all()
+
+with app.app_context():
+    east_point = Hospital(
+        name="East Point Hospital",
+        address="3P39+M3 Bengaluru, Karnataka",
+        city="Bangalore",
+        rating=4.8,
+        specialization="Multi-specialty",
+        cost_category=2,
+        specialists=["Cardiology", "Neurology", "Orthopedics", "Dermatology"],
+        symptoms=["Chest Pain", "Headache", "Skin Allergy", "Joint Pain"]
+    )
+    db.session.add(east_point)
+    db.session.commit()
+    print("Hospital added successfully!")
+
 
 
 
