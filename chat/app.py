5c69a3dd11e481e7ca26ca4a97e06f1ac30f027a
VeerendraDidagur <1ep23is181@eastpoint.ac.in>
2025-11-24 13:32:11 +0530

    Update app.py

--- chat/app.py
+++ chat/app.py
@@ -182,8 +182,13 @@ def success():
 # -------------------------
 # RUN
 # -------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
 
 
