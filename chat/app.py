b5487fe179e337a1cd0eb07e6a0361fbdf2fd653
VeerendraDidagur <1ep23is181@eastpoint.ac.in>
2025-11-24 14:32:52 +0530

    Update app.py

--- chat/app.py
+++ chat/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # -----------------------------
 # Sample hospital + doctors data
@@ -197,6 +197,7 @@ def success():
 # -------------------------
 # RUN
 # -------------------------
-if _name_ == "_main_":
+if __name__ == "_main_":
     app.run(host="0.0.0.0", port=10000)
+
 
